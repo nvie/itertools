c9b176dd239b2729b219115a9738fdf8cab40101
Vincent Driessen <me@nvie.com>
2017-12-16 09:33:21 +0100

    Stop using "any"'s

--- src/itertools.js
+++ src/itertools.js
@@ -257,8 +257,9 @@ export function* izipMany<T>(...iters: Array<Iterable<T>>): Iterable<Array<T>> {
     const iterables = iters.map(iter);
 
     for (;;) {
-        const heads: Array<IteratorResult<T, any>> = iterables.map(xs => xs.next());
+        const heads: Array<IteratorResult<T, *>> = iterables.map(xs => xs.next());
         if (all(heads, h => !h.done)) {
+            // $FlowFixMe
             yield heads.map(h => ((h.value: any): T));
         } else {
             // One of the iterables exhausted
