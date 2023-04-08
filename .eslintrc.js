776712755426cc79fbdec53091397163bb967e98
Vincent Driessen <me@nvie.com>
2023-04-08 09:30:48 +0200

    Fix ESLint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,23 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
     root: true,
+
+    rules: {
+        '@typescript-eslint/no-unused-vars': [
+            'warn',
+            // Unused variables are fine if they start with an underscore
+            { args: 'all', argsIgnorePattern: '^_.*', varsIgnorePattern: '^_.*' },
+        ],
+    },
+
+    overrides: [
+        {
+            files: ['src/**/__tests__/**'],
+
+            // Special config for test files
+            rules: {
+                '@typescript-eslint/no-non-null-assertion': 'off',
+            },
+        },
+    ],
 };
