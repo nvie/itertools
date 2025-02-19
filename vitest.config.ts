8cd53464cfd97d063e4775a161acaa9fb26cf6f9
Vincent Driessen <me@nvie.com>
2025-02-19 14:43:40 +0100

    Upgrade Vitest to v3

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,10 +9,12 @@ export default defineConfig({
       reporter: ["text", "html"],
 
       // Require 100% test coverage
-      lines: 100,
-      functions: 100,
-      statements: 100,
-      branches: 100,
+      thresholds: {
+        lines: 100,
+        functions: 100,
+        statements: 100,
+        branches: 100,
+      },
     },
   },
 });
